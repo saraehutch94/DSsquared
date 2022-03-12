f8542f94e777761693138d4a88f97d4c8329b6d9
scottklein7 <scott.klein02@protonmail.com>
2022-03-12 16:59:35 -0500

    Adds: post body for search page and results page for lyrics and artist

--- music/urls.py
+++ music/urls.py
@@ -5,5 +5,7 @@ from music import views
 urlpatterns = [
     path('', views.get_home, name='home'),
     path('search-artist-title/', views.search_artist_title, name='search_artist_title'),
+    path('artist-title-result/<str:artist_title>/', views.get_artist_title_result, name='artist_title_result'),
     path('search-lyric/', views.search_lyric, name='search_lyric'),
+    path('lyric-result/<str:lyrics>/', views.lyric_result, name='lyric_result'),
 ]
--- music/views.py
+++ music/views.py
@@ -1,13 +1,39 @@
 from django.shortcuts import render, redirect
-
+from .forms import ArtistTitle, LyricForm
 # Create your views here.
 
 
 def get_home(request):
     return render(request, 'home.html')
 
+
 def search_artist_title(request):
-    return render(request, 'search_content/search_artist_title.html')
+    form = ArtistTitle()
+    if request.method == "POST":
+        form = ArtistTitle(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            artist_title = cd.get('artist_title')
+            print(cd)
+            return redirect('artist_title_result', artist_title = artist_title)
+
+    return render(request, 'search_content/search_artist_title.html', {'form': form})
     
+
+def get_artist_title_result(request, artist_title):
+    return render(request, 'search_content/search_lyric.html', {'artist_title': artist_title})
+
+
 def search_lyric(request):
-    return render(request, 'search_content/search_lyric.html')
+    form = LyricForm()
+    if request.method == "POST":
+        form = LyricForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            lyrics = cd.get('lyrics')
+            return redirect('lyric_result', lyrics = lyrics)
+    return render(request, 'search_content/search_lyric.html', {'form': form})
+
+
+def lyric_result(request, lyrics):
+    return render(request, 'search_results/lyric_result.html', {'lyrics': lyrics}) 
