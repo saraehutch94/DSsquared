4d08255fcb40ad111acdc9f97540c900bc722471
Sara Hutchinson <sarahutchinson@Saras-MacBook-Pro-2.local>
2022-03-12 17:11:28 -0500

    call to genius API in lyric_result

--- music/views.py
+++ music/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, redirect
 from .forms import LyricForm
 
+import lyricsgenius
+client_access_token = "kLxktps7zneNyDod1aF4g-Uy_RDBblfNvL-CK-aACZzVJ6dXNNhqgQLfq-v6q3od"
+genius = lyricsgenius.Genius(client_access_token)
+
 # Create your views here.
 
 
@@ -21,4 +25,19 @@ def search_lyric(request):
     return render(request, 'search_content/search_lyric.html', {'form': form})
 
 def lyric_result(request, lyrics):
-    return render(request, 'search_results/lyric_result.html', {'lyrics': lyrics})
+    lyric_search = genius.search_lyrics(search_term=lyrics, per_page=1, page=1)
+    song_title = lyric_search["sections"][0]["hits"][0]["result"]["title"]
+    song_artist = lyric_search["sections"][0]["hits"][0]["result"]["primary_artist"]["name"]
+    search_song_lyrics = genius.search_song(title=song_title, artist=song_artist)
+    song_lyrics = search_song_lyrics.lyrics
+    song_id = lyric_search["sections"][0]["hits"][0]["result"]["id"]
+    song = genius.song(song_id=song_id)
+    song_description = song["song"]["description"]["plain"]
+    song_art_image_url = song["song"]["song_art_image_url"]
+    return render(request, 'search_results/lyric_result.html', {
+        'song_lyrics': song_lyrics,
+        'song_title': song_title,
+        'song_artist': song_artist,
+        'song_description': song_description,
+        'song_art_image_url': song_art_image_url
+    })
