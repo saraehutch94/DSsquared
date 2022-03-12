a2c99bbde47a7572068dbdf8bb31a3e6d7cf8981
Sara Hutchinson <sarahutchinson@Saras-MacBook-Pro-2.local>
2022-03-12 16:03:26 -0500

    attach post body to lyrics_result

--- music/urls.py
+++ music/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.get_home, name='home'),
     path('search-artist-title/', views.search_artist_title, name='search_artist_title'),
     path('search-lyric/', views.search_lyric, name='search_lyric'),
+    path('lyric-result/<str:lyrics>/', views.lyric_result, name='lyric_result'),
 ]
--- music/views.py
+++ music/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from .forms import LyricForm
 
 # Create your views here.
 
@@ -10,4 +11,14 @@ def search_artist_title(request):
     return render(request, 'search_content/search_artist_title.html')
     
 def search_lyric(request):
-    return render(request, 'search_content/search_lyric.html')
+    form = LyricForm()
+    if request.method == "POST":
+        form = LyricForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            lyrics = cd.get('lyrics')
+            return redirect('lyric_result', lyrics = lyrics)
+    return render(request, 'search_content/search_lyric.html', {'form': form})
+
+def lyric_result(request, lyrics):
+    return render(request, 'search_results/lyric_result.html', {'lyrics': lyrics})
