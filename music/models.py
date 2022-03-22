84adc3a6a93dceec6bfff411fd568989f268fedd
Sara Hutchinson <sarahutchinson@Saras-MacBook-Pro-2.local>
2022-03-22 19:46:24 -0400

    push to new branch

--- music/models.py
+++ music/models.py
@@ -2,9 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
 
-# comment for pull request
-
-
 class ArtistRating(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     artist_img = models.CharField(max_length=200)
