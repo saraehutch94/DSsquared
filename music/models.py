8a58875384b64278d2527c024bb1e964e3c15e8d
Scott Klein <81238878+scottklein7@users.noreply.github.com>
2022-03-22 19:03:57 -0400

    Adds sara pull req

--- music/models.py
+++ music/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 class ArtistRating(models.Model):
@@ -9,6 +10,11 @@ class ArtistRating(models.Model):
     artist_songs = models.TextField(max_length=500)
     rating = models.IntegerField()
 
+    def __str__(self):
+        return f'user: {self.user} | artist_img: {self.artist_img} | artist_name: {self.artist_name} | artist_songs: {self.artist_songs} | rating: {self.rating}'
+
+    def get_absolute_url(self):
+        return reverse('artist_rating_detail', kwargs={'rating_id': self.id, 'user_id': self.user_id})
 
 class LyricRatings(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -17,7 +23,13 @@ class LyricRatings(models.Model):
     song_artist = models.CharField(max_length=100)
     song_description = models.TextField(max_length=500)
     song_art_image_url = models.CharField(max_length=200)
+    rating = models.IntegerField()
 
+    def __str__(self):
+        return f'user: {self.user} | lyrics: {self.song_lyrics} | title: {self.song_title} | artist: {self.song_artist} | description: {self.song_description} | image: {self.song_art_image_url} | rating: {self.rating}'
+    
+    def get_absolute_url(self):
+        return reverse('lyric_rating_detail', kwargs={'rating_id': self.id, 'user_id': self.user_id})
 
 class SongRating(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -25,3 +37,10 @@ class SongRating(models.Model):
     song_lyrics = models.TextField(max_length=500)
     song_art_image = models.CharField(max_length=100)
     song_artist = models.CharField(max_length=100)
+    rating = models.IntegerField()
+
+    def __str__(self):
+        return f'user: {self.user} | song_title: {self.song_title} | song_lyrics: {self.song_lyrics} | song_art_image: {self.song_art_image} | song_artist: {self.song_artist} | rating: {self.rating}'
+
+    def get_absolute_url(self):
+        return reverse('song_rating_detail', kwargs={'rating_id': self.id, 'user_id': self.user_id})
